d4ff551445a8a2d632cdc3666868b246770e3f77
thiago.oliveira <thiago.oliveira@intera.org.br>
2021-08-25 01:49:57 -0400

    add unform implementation in inputs form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,4 +1,6 @@
 import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react';
+import { useField } from '@unform/core';
+import React, { useEffect, useRef } from 'react'; 
 
 interface InputProps extends ChakraInputProps {
     name: string;
@@ -6,13 +8,26 @@ interface InputProps extends ChakraInputProps {
 } 
 
 export function Input({ name, label, ...rest }: InputProps) {
+    const inputRef = useRef(null);
+    const { fieldName, registerField, defaultValue, error } = useField(name);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value'
+        })
+    }, [fieldName, registerField]);
+
     return (
         <FormControl>
             { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
             
             <ChakraInput 
+                ref={inputRef}
                 name={name}
                 id={name}
+                defaultValue={defaultValue}
                 focusBorderColor="pink.500"
                 bgColor="gray.900"
                 variant="filled"
--- src/components/Form/Textarea.tsx
+++ src/components/Form/Textarea.tsx
@@ -1,4 +1,6 @@
 import { Textarea as ChakraTextarea, FormLabel, FormControl, TextareaProps as ChakraTextareaProps } from '@chakra-ui/react';
+import { useField } from '@unform/core';
+import React, { useEffect, useRef } from 'react'; 
 
 interface TextareaProps extends ChakraTextareaProps {
     name: string;
@@ -6,13 +8,26 @@ interface TextareaProps extends ChakraTextareaProps {
 } 
 
 export function Textarea({ name, label, ...rest }: TextareaProps) {
+    const inputRef = useRef(null);
+    const { fieldName, registerField, defaultValue, error } = useField(name);
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value'
+        })
+    }, [fieldName, registerField]);
+
     return (
         <FormControl>
             { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
             
-            <ChakraTextarea 
+            <ChakraTextarea
+                ref={inputRef} 
                 name={name}
                 id={name}
+                defaultValue={defaultValue}
                 focusBorderColor="pink.500"
                 bgColor="gray.900"
                 variant="filled"
